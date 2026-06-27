3d9f238d7c77e98b31b32e1670293fca4095c6b7
Mukhtorov Ibrohim <mukhtorovibrohim209@gmail.com>
2026-06-27 19:16:55 +0500

    a

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,7 +2,6 @@ import { lazy } from "react";
 
 export const Login = lazy(() => import("../pages/Login/Login"));
 export const Dashboard = lazy(() => import("../pages/Dashboard/Dashboard"));
-export const Users = lazy(() => import("../pages/Users/Users"));
 export const Product = lazy(() => import("../pages/Product/Product"));
 export const AddProductDetails = lazy(() => import("../pages/Product/AddProductDetails"));
 export const EditProductDetails = lazy(() => import("../pages/Product/EditProductDetails"));
